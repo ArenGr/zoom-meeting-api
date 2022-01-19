6724aca5fb98ef4d883ae4b0e165d931c751420f
ArenGr <47744223+ArenGr@users.noreply.github.com>
2022-01-20 02:18:39 +0400

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 /* mysql database credentials */ 
 define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'admin_30');
-define('DB_PASSWORD', 'areN_1990');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'zoom');
 
 /* zoom app credentials */
-define('API_KEY', 'llnaglZFTduX0n27ymeqvw');
-define('API_SECRET', 'xEetv1wBQsfw1KcO8HaPOMH4K2SJZbw3hGQw');
+define('API_KEY', '');
+define('API_SECRET', '');
 
 
 /* sendgrid */
-define('MAIL_API_KEY', "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");
-define('MAIL_FROM_ADDRESS', 'info@coderiders.am'); //should match a verified Sender Identity in Sendgrid
+define('MAIL_API_KEY', "");
+define('MAIL_FROM_ADDRESS', ''); //should match a verified Sender Identity in Sendgrid
 
 /*Do NOT change*/
 define('MAIL_URL', 'https://api.sendgrid.com/v3/mail/send');
